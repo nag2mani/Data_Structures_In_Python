340a3b1b0b959cd1db35906f8775a854940f058f
Nagmani kumar <nag2mani@gmail.com>
2023-10-23 19:27:55 +0530

    BottomUpHeapConstruction class added

--- heap_priority_queue.py
+++ heap_priority_queue.py
@@ -18,18 +18,18 @@ class HeapPriorityQueue(PriorityQueueBase): # base class defines Item
         return 2 * j + 2
 
     def _has_left(self, j):
-        return self._left(j) < len(self.data)  # index beyond the end of the list?
+        return self._left(j) < len(self._data)  # index beyond the end of the list?
 
     def _has_right(self, j):
-        return self._right(j) < len(self.data)  # index beyond the end of the list?
+        return self._right(j) < len(self._data)  # index beyond the end of the list?
 
     def swap(self, i, j):
         """Swap the elements at indices i and j of the array."""
-        self.data[i], self.data[j] = self.data[j], self.data[i]
+        self._data[i], self._data[j] = self._data[j], self._data[i]
 
     def _upheap(self, j):
         parent = self._parent(j)
-        if j > 0 and self.data[j] < self.data[parent]:
+        if j > 0 and self._data[j] < self._data[parent]:
             self.swap(j, parent)
             self._upheap(parent)  # recur at the position of the parent
 
@@ -39,24 +39,24 @@ class HeapPriorityQueue(PriorityQueueBase): # base class defines Item
             small_child = left  # although right may be smaller
             if self._has_right(j):
                 right = self._right(j)
-                if self.data[right] < self.data[left]:
+                if self._data[right] < self._data[left]:
                     small_child = right
-            if self.data[small_child] < self.data[j]:
+            if self._data[small_child] < self._data[j]:
                 self.swap(j, small_child)
                 self._downheap(small_child)  # recur at the position of the small child
 
     def __init__(self):
         """Create a new empty Priority Queue."""
-        self.data = []
+        self._data = []
 
     def __len__(self):
         """Return the number of items in the priority queue."""
-        return len(self.data)
+        return len(self._data)
 
     def add(self, key, value):
         """Add a key-value pair to the priority queue."""
-        self.data.append(self._Item(key, value))
-        self._upheap(len(self.data) - 1)  # _upheap newly added position
+        self._data.append(self._Item(key, value))
+        self._upheap(len(self._data) - 1)  # _upheap newly added position
 
     def min(self):
         """Return but do not remove (k,v) tuple with the minimum key.
@@ -65,7 +65,7 @@ class HeapPriorityQueue(PriorityQueueBase): # base class defines Item
         """
         if self.is_empty():
             raise "Priority queue is empty."
-        item = self.data[0]
+        item = self._data[0]
         return (item._key, item._value)
 
     def remove_min(self):
@@ -75,8 +75,8 @@ class HeapPriorityQueue(PriorityQueueBase): # base class defines Item
         """
         if self.is_empty():
             raise "Priority queue is empty."
-        self.swap(0, len(self.data) - 1)  # put the minimum item at the end
-        item = self.data.pop()  # and remove it from the list;
+        self.swap(0, len(self._data) - 1)  # put the minimum item at the end
+        item = self._data.pop()  # and remove it from the list;
         self._downheap(0)  # then fix the new root
         return (item._key, item._value)
 
@@ -97,3 +97,34 @@ if __name__ == "__main__":
     print(H.min())
 
 
+
+class BottomUpHeapConstruction(HeapPriorityQueue):
+
+    def __init__(self, contents = ()):
+        """Create a new priority queue.By default, queue will be empty. If contents is given, it should be as an iterable sequence of (k,v) tuples specifying the initial contents."""
+        self._data = [self._Item(k,v) for k , v in contents]
+        if len(self._data) > 1:
+            self._heapify()
+    
+    def _heapify(self):
+        start = self._parent(len(self) - 1)
+        for i in range(start, -1, -1):
+            self._downheap(i)
+
+
+if __name__ == "__main__":
+    print("BottomUpHeapConstruction Start: ")
+    B = BottomUpHeapConstruction()
+    B.add(2, "j")
+    B.add(8, "j8")
+    B.add(90, "j8")
+    B.add(1, "j1")
+    B.add(12, "j1")
+    B.add(0, "j1")
+
+    print(B.remove_min())
+    print(B.remove_min())
+    print(B.remove_min())
+    print(B.remove_min())
+    print(B.remove_min())
+    print(B.remove_min())
