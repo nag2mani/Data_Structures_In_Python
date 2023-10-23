77bbb59f91670a551dc5b0740ab37a4cf8dee9e1
Nagmani kumar <nag2mani@gmail.com>
2023-10-23 18:13:08 +0530

    added some print statement to test the code

--- heap_priority_queue.py
+++ heap_priority_queue.py
@@ -27,13 +27,13 @@ class HeapPriorityQueue(PriorityQueueBase): # base class defines Item
         """Swap the elements at indices i and j of the array."""
         self.data[i], self.data[j] = self.data[j], self.data[i]
 
-    def upheap(self, j):
+    def _upheap(self, j):
         parent = self._parent(j)
         if j > 0 and self.data[j] < self.data[parent]:
             self.swap(j, parent)
-            self.upheap(parent)  # recur at the position of the parent
+            self._upheap(parent)  # recur at the position of the parent
 
-    def downheap(self, j):
+    def _downheap(self, j):
         if self._has_left(j):
             left = self._left(j)
             small_child = left  # although right may be smaller
@@ -43,7 +43,7 @@ class HeapPriorityQueue(PriorityQueueBase): # base class defines Item
                     small_child = right
             if self.data[small_child] < self.data[j]:
                 self.swap(j, small_child)
-                self.downheap(small_child)  # recur at the position of the small child
+                self._downheap(small_child)  # recur at the position of the small child
 
     def __init__(self):
         """Create a new empty Priority Queue."""
@@ -56,7 +56,7 @@ class HeapPriorityQueue(PriorityQueueBase): # base class defines Item
     def add(self, key, value):
         """Add a key-value pair to the priority queue."""
         self.data.append(self._Item(key, value))
-        self.upheap(len(self.data) - 1)  # upheap newly added position
+        self._upheap(len(self.data) - 1)  # _upheap newly added position
 
     def min(self):
         """Return but do not remove (k,v) tuple with the minimum key.
@@ -77,7 +77,7 @@ class HeapPriorityQueue(PriorityQueueBase): # base class defines Item
             raise "Priority queue is empty."
         self.swap(0, len(self.data) - 1)  # put the minimum item at the end
         item = self.data.pop()  # and remove it from the list;
-        self.downheap(0)  # then fix the new root
+        self._downheap(0)  # then fix the new root
         return (item._key, item._value)
 
 
@@ -85,12 +85,15 @@ if __name__ == "__main__":
     H = HeapPriorityQueue()
     H.add(2, "j")
     H.add(8, "j8")
+    H.add(90, "j8")
     H.add(1, "j1")
+    H.add(12, "j1")
+    H.add(0, "j1")
 
     print(len(H))
     print(H.remove_min())
     print(H.remove_min())
     print(len(H))
-
+    print(H.min())
 
 
