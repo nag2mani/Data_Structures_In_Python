49e5f0b7bc4efa433601b3970112f951f14e07d2
Nagmani kumar <nag2mani@gmail.com>
2023-11-01 19:48:56 +0530

    python heap impliment

--- python_heap.py
+++ python_heap.py
@@ -1,2 +1,15 @@
-import heapq
+from heapq import *
+
+count = 0
+
+class StackByQueue:
+    def __init__(self):
+        self.heap = []
+        heapify(self.heap)
+    def push(self, v):
+        global count
+        count += 1
+        heappush(self.heap, (-1 * count, v))
+    def pop(self):
+        return heappop(self.heap)
 
